e773f695cbf514de24568a82f95127b6d14c13a4
macha <macha1405@yahoo.co.jp>
2021-01-11 20:37:38 +0900

    add

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Family, Result
 # ...
 admin.site.register(User)
+admin.site.register(Family)
+admin.site.register(Result)
--- registration/models.py
+++ registration/models.py
@@ -4,3 +4,19 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField('メールアドレス', unique=True)
+
+
+class Family(models.Model):
+    family_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    person = models.CharField(verbose_name='利用者', max_length=255)
+
+    def __str__(self):
+            return self.person
+
+
+class Result(models.Model):
+    result_family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    temperature = models.FloatField(verbose_name='体温')
+
+    def __str__(self):
+            return self.temperature
